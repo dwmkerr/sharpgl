0cc2e18074fba2329b3940a7b51b24f2ec7965ff
Dave Kerr <dwmkerr@gmail.com>
2014-03-15 01:25:31 +0100

    Added the 'modern opengl sample' project, which will demonstrate vertex buffers and shaders. Now preparing to work on the new shader class, which will be at the core sharpgl level (marking the beginning of the move away from the scenegraph).

---
+++ source/SharpGL/Samples/WinForms/ModernOpenGLSample/FormModernOpenGLSample.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ModernOpenGLSample
+{
+    partial class FormModernOpenGLSample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLControl1 = new SharpGL.OpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.DrawFPS = false;
+            this.openGLControl1.Location = new System.Drawing.Point(12, 12);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl1.RenderContextType = SharpGL.RenderContextType.NativeWindow;
+            this.openGLControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl1.Size = new System.Drawing.Size(661, 365);
+            this.openGLControl1.TabIndex = 0;
+            this.openGLControl1.OpenGLInitialized += new System.EventHandler(this.openGLControl1_OpenGLInitialized);
+            this.openGLControl1.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl1_OpenGLDraw);
+            // 
+            // FormModernOpenGLSample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 389);
+            this.Controls.Add(this.openGLControl1);
+            this.Name = "FormModernOpenGLSample";
+            this.Text = "Modern OpenGL Sample";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl1;
+
+
+    }
+}
+
---
+++ source/SharpGL/Samples/WinForms/ModernOpenGLSample/FormModernOpenGLSample.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SharpGL;
+
+namespace ModernOpenGLSample
+{
+    public partial class FormModernOpenGLSample : Form
+    {
+        public FormModernOpenGLSample()
+        {
+            InitializeComponent();
+        }
+
+        private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
+        {
+            var gl = openGLControl1.OpenGL;
+
+            gl.ClearColor(0.4f, 0.6f, 0.9f, 0.0f);
+        }
+
+        private void openGLControl1_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            var gl = openGLControl1.OpenGL;
+
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_STENCIL_BUFFER_BIT);
+            DrawShapes(gl);
+        }
+
+        private void DrawShapes(OpenGL gl)
+        {
+            gl.LoadIdentity();					// Reset The View
+
+            // gl.Color(1.0f, 1.0f, 1.0f);
+            // gl.FontBitmaps.DrawText(gl, 0, 0, "Arial", "Argh");
+
+
+
+            gl.Translate(-1.5f, 0.0f, -6.0f);				// Move Left And Into The Screen
+
+            gl.Begin(OpenGL.GL_TRIANGLES);					// Start Drawing The Pyramid
+
+            gl.Color(1.0f, 0.0f, 0.0f);			// Red
+            gl.Vertex(0.0f, 1.0f, 0.0f);			// Top Of Triangle (Front)
+            gl.Color(0.0f, 1.0f, 0.0f);			// Green
+            gl.Vertex(-1.0f, -1.0f, 1.0f);			// Left Of Triangle (Front)
+            gl.Color(0.0f, 0.0f, 1.0f);			// Blue
+            gl.Vertex(1.0f, -1.0f, 1.0f);			// Right Of Triangle (Front)
+
+            gl.Color(1.0f, 0.0f, 0.0f);			// Red
+            gl.Vertex(0.0f, 1.0f, 0.0f);			// Top Of Triangle (Right)
+            gl.Color(0.0f, 0.0f, 1.0f);			// Blue
+            gl.Vertex(1.0f, -1.0f, 1.0f);			// Left Of Triangle (Right)
+            gl.Color(0.0f, 1.0f, 0.0f);			// Green
+            gl.Vertex(1.0f, -1.0f, -1.0f);			// Right Of Triangle (Right)
+
+            gl.Color(1.0f, 0.0f, 0.0f);			// Red
+            gl.Vertex(0.0f, 1.0f, 0.0f);			// Top Of Triangle (Back)
+            gl.Color(0.0f, 1.0f, 0.0f);			// Green
+            gl.Vertex(1.0f, -1.0f, -1.0f);			// Left Of Triangle (Back)
+            gl.Color(0.0f, 0.0f, 1.0f);			// Blue
+            gl.Vertex(-1.0f, -1.0f, -1.0f);			// Right Of Triangle (Back)
+
+            gl.Color(1.0f, 0.0f, 0.0f);			// Red
+            gl.Vertex(0.0f, 1.0f, 0.0f);			// Top Of Triangle (Left)
+            gl.Color(0.0f, 0.0f, 1.0f);			// Blue
+            gl.Vertex(-1.0f, -1.0f, -1.0f);			// Left Of Triangle (Left)
+            gl.Color(0.0f, 1.0f, 0.0f);			// Green
+            gl.Vertex(-1.0f, -1.0f, 1.0f);			// Right Of Triangle (Left)
+            gl.End();						// Done Drawing The Pyramid
+
+            gl.LoadIdentity();
+            gl.Translate(1.5f, 0.0f, -7.0f);				// Move Right And Into The Screen
+
+            gl.Begin(OpenGL.GL_QUADS);					// Start Drawing The Cube
+
+            gl.Color(0.0f, 1.0f, 0.0f);			// Set The Color To Green
+            gl.Vertex(1.0f, 1.0f, -1.0f);			// Top Right Of The Quad (Top)
+            gl.Vertex(-1.0f, 1.0f, -1.0f);			// Top Left Of The Quad (Top)
+            gl.Vertex(-1.0f, 1.0f, 1.0f);			// Bottom Left Of The Quad (Top)
+            gl.Vertex(1.0f, 1.0f, 1.0f);			// Bottom Right Of The Quad (Top)
+
+
+            gl.Color(1.0f, 0.5f, 0.0f);			// Set The Color To Orange
+            gl.Vertex(1.0f, -1.0f, 1.0f);			// Top Right Of The Quad (Bottom)
+            gl.Vertex(-1.0f, -1.0f, 1.0f);			// Top Left Of The Quad (Bottom)
+            gl.Vertex(-1.0f, -1.0f, -1.0f);			// Bottom Left Of The Quad (Bottom)
+            gl.Vertex(1.0f, -1.0f, -1.0f);			// Bottom Right Of The Quad (Bottom)
+
+            gl.Color(1.0f, 0.0f, 0.0f);			// Set The Color To Red
+            gl.Vertex(1.0f, 1.0f, 1.0f);			// Top Right Of The Quad (Front)
+            gl.Vertex(-1.0f, 1.0f, 1.0f);			// Top Left Of The Quad (Front)
+            gl.Vertex(-1.0f, -1.0f, 1.0f);			// Bottom Left Of The Quad (Front)
+            gl.Vertex(1.0f, -1.0f, 1.0f);			// Bottom Right Of The Quad (Front)
+
+            gl.Color(1.0f, 1.0f, 0.0f);			// Set The Color To Yellow
+            gl.Vertex(1.0f, -1.0f, -1.0f);			// Bottom Left Of The Quad (Back)
+            gl.Vertex(-1.0f, -1.0f, -1.0f);			// Bottom Right Of The Quad (Back)
+            gl.Vertex(-1.0f, 1.0f, -1.0f);			// Top Right Of The Quad (Back)
+            gl.Vertex(1.0f, 1.0f, -1.0f);			// Top Left Of The Quad (Back)
+
+            gl.Color(0.0f, 0.0f, 1.0f);			// Set The Color To Blue
+            gl.Vertex(-1.0f, 1.0f, 1.0f);			// Top Right Of The Quad (Left)
+            gl.Vertex(-1.0f, 1.0f, -1.0f);			// Top Left Of The Quad (Left)
+            gl.Vertex(-1.0f, -1.0f, -1.0f);			// Bottom Left Of The Quad (Left)
+            gl.Vertex(-1.0f, -1.0f, 1.0f);			// Bottom Right Of The Quad (Left)
+
+            gl.Color(1.0f, 0.0f, 1.0f);			// Set The Color To Violet
+            gl.Vertex(1.0f, 1.0f, -1.0f);			// Top Right Of The Quad (Right)
+            gl.Vertex(1.0f, 1.0f, 1.0f);			// Top Left Of The Quad (Right)
+            gl.Vertex(1.0f, -1.0f, 1.0f);			// Bottom Left Of The Quad (Right)
+            gl.Vertex(1.0f, -1.0f, -1.0f);			// Bottom Right Of The Quad (Right)
+            gl.End();						// Done Drawing The Q
+
+            gl.Flush();
+        }
+    }
+}
