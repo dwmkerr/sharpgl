76a0ad1580dbef9c7f200a6896590c4329d1550f
Dave Kerr <dwmkerr@gmail.com>
2014-04-05 11:54:17 +0200

    Finished ARB_compute_shader

--- source/SharpGL/Core/SharpGL/OpenGLExtensions.cs
+++ source/SharpGL/Core/SharpGL/OpenGLExtensions.cs
@@ -4950,5 +4950,53 @@ namespace SharpGL
         private delegate void glClearNamedBufferSubDataEXT(uint buffer, uint internalformat, IntPtr offset, uint size, uint format, uint type, IntPtr data);
 
         #endregion
+
+        #region GL_ARB_compute_shader
+
+        /// <summary>
+        /// Launch one or more compute work groups
+        /// </summary>
+        /// <param name="num_groups_x">The number of work groups to be launched in the X dimension.</param>
+        /// <param name="num_groups_y">The number of work groups to be launched in the Y dimension.</param>
+        /// <param name="num_groups_z">The number of work groups to be launched in the Z dimension.</param>
+        public void DispatchCompute(uint num_groups_x, uint num_groups_y, uint num_groups_z)
+        {
+            InvokeExtensionFunction<glDispatchCompute>(num_groups_x, num_groups_y, num_groups_z);
+        }
+
+        /// <summary>
+        /// Launch one or more compute work groups using parameters stored in a buffer
+        /// </summary>
+        /// <param name="indirect">The offset into the buffer object currently bound to the GL_DISPATCH_INDIRECT_BUFFER​ buffer target at which the dispatch parameters are stored.</param>
+        public void DispatchComputeIndirect(IntPtr indirect)
+        {
+            InvokeExtensionFunction<glDispatchComputeIndirect>(indirect);
+        }
+
+        //  Delegates
+        private delegate void glDispatchCompute(uint num_groups_x, uint num_groups_y, uint num_groups_z);
+        private delegate void glDispatchComputeIndirect(IntPtr indirect);
+
+        // Constants
+        public const uint GL_COMPUTE_SHADER = 0x91B9;
+        public const uint GL_MAX_COMPUTE_UNIFORM_BLOCKS = 0x91BB;
+        public const uint GL_MAX_COMPUTE_TEXTURE_IMAGE_UNITS = 0x91BC;
+        public const uint GL_MAX_COMPUTE_IMAGE_UNIFORMS = 0x91BD;
+        public const uint GL_MAX_COMPUTE_SHARED_MEMORY_SIZE = 0x8262;
+        public const uint GL_MAX_COMPUTE_UNIFORM_COMPONENTS = 0x8263;
+        public const uint GL_MAX_COMPUTE_ATOMIC_COUNTER_BUFFERS = 0x8264;
+        public const uint GL_MAX_COMPUTE_ATOMIC_COUNTERS = 0x8265;
+        public const uint GL_MAX_COMBINED_COMPUTE_UNIFORM_COMPONENTS = 0x8266;
+        public const uint GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS = 0x90EB;
+        public const uint GL_MAX_COMPUTE_WORK_GROUP_COUNT = 0x91BE;
+        public const uint GL_MAX_COMPUTE_WORK_GROUP_SIZE = 0x91BF;
+        public const uint GL_COMPUTE_WORK_GROUP_SIZE = 0x8267;
+        public const uint GL_UNIFORM_BLOCK_REFERENCED_BY_COMPUTE_SHADER = 0x90EC;
+        public const uint GL_ATOMIC_COUNTER_BUFFER_REFERENCED_BY_COMPUTE_SHADER = 0x90ED;
+        public const uint GL_DISPATCH_INDIRECT_BUFFER = 0x90EE;
+        public const uint GL_DISPATCH_INDIRECT_BUFFER_BINDING = 0x90EF;
+        public const uint GL_COMPUTE_SHADER_BIT = 0x00000020;
+
+        #endregion
     }
 }
