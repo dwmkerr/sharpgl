b28817ee338396ec91e63dc9736e19f7492aeb2a
Moritz Grosch <moritz.grosch@iwu.fraunhofer.de>
2014-04-09 17:09:00 +0200

    Added Sample for NV_DX_interop (FastGL)
    The sample has some known limitations:
     - resizing the scene doesn't work properly yet
     - Code has to be cleaned, I extracted it from our Codebase
     - probally a lot of yet unknown bugs, DirectX can be an asshole and there
       is little to know documentation for NV_DX_interop (and even what exists
       is incomplete ...)
    
    This is only working on GPUs supporting DirectX9 and only in Windows Vista
    and newer. The Windows-version-issue can be fixed with some work (using
    Device on Windows older than Vista and DeviceEx on Vista and newer).

---
+++ source/SharpGL/Samples/WPF/FastGLControl.WPF/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FastGL
+{
+	/// <summary>
+	/// Interaktionslogik für MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			fastGLControl1.Render += new FastGLControl.RenderHandler(fastGLControl1_Render);
+		}
+
+		void fastGLControl1_Render(object sender, FastGLControl.RenderEventArgs args)
+		{
+			args.GL.Begin(SharpGL.Enumerations.BeginMode.Triangles);
+			
+			args.GL.Color(1f, 0f, 0f);
+			args.GL.Vertex(200, 10);
+
+			args.GL.Color(0f, 1f, 0f);
+			args.GL.Vertex(100, 200);
+
+			args.GL.Color(0f, 0f, 1f);
+			args.GL.Vertex(300, 200);
+			
+			args.GL.End();
+		}
+	}
+}
