dc301b694f30cfa73b7444b6781a7361d0f81a1d
Robin Sedlaczek <robin.sedlaczek@live.de>
2014-08-17 18:21:30 +0200

    Change solution to 2013 and add some performance measurements to CelShadingSample.

---
+++ source/SharpGL/Tests/SharpGL.PerformanceTest/PerformanceTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace SharpGL.PerformanceTest
+{
+    [TestFixture]
+    public class PerformanceTests
+    {
+        [TestFixtureSetUp]
+        public void TestSetUp()
+        {
+
+        }
+
+        [TestFixtureTearDown]
+        public void TestTearDown()
+        {
+            
+        }
+
+        [Test]
+        public void Test()
+        {
+
+
+        }
+
+
+    }
+}
