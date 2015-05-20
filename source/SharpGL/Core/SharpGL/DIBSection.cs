8eab6881a62afd80d2cbe4bb5bbcaadd3ba0b89b
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-05-21 01:39:48 +0800

    add xml note to DIBSection

--- source/SharpGL/Core/SharpGL/DIBSection.cs
+++ source/SharpGL/Core/SharpGL/DIBSection.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 namespace SharpGL
 {
     /// <summary>
-    /// 
+    /// device-independent bitmap.
     /// </summary>
 	public class DIBSection : IDisposable
 	{
