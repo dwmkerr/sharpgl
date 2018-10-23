fef7b0af9313f11bc102adb79490b80393716d23
Dave Kerr <dwmkerr@gmail.com>
2018-10-23 09:32:58 +0100

    fix: fixed a bug which caused GLColor to incorrectly clamp values

--- source/SharpGL/Core/SharpGL.SceneGraph/GLColor.cs
+++ source/SharpGL/Core/SharpGL.SceneGraph/GLColor.cs
@@ -68,10 +68,10 @@ namespace SharpGL.SceneGraph
 			if(g>1)g=1;
 			if(b>1)b=1;
 			if(a>1)a=1;
-			if(r<1)r=0;
-			if(g<1)g=0;
-			if(b<1)b=0;
-			if(a<1)a=0;
+			if(r<0)r=0;
+			if(g<0)g=0;
+			if(b<0)b=0;
+			if(a<0)a=0;
 		}
 
 		protected float r = 0.0f;
