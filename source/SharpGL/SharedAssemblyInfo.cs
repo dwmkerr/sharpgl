4ef4a4852e0944adce90c373e8593c67a0a35848
Dave Kerr <dwmkerr@gmail.com>
2023-04-28 12:55:57 +0800

    fix: annotate version file for release please

--- source/SharpGL/SharedAssemblyInfo.cs
+++ source/SharpGL/SharedAssemblyInfo.cs
@@ -15,5 +15,11 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+//
+// Note that the 'x-release-please-' comments are used to let the release-please
+// action know that we want to update semvers on these lines. Please do not
+// remove these comments.
+// x-release-please-start-version
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
+// x-release-please-end
